20f20d8a43a7a95eec4d6e6971a0c9026d030625
Raul Bernal <bernal.raul@gmail.com>
2024-01-24 13:05:02 +0100

    Create create_database_schema.py
    Simple python script to init the local database file.
    Could be used to launch other simple queries, look the last 6 lines.

---
+++ create_database_schema.py
@@ -0,0 +1,19 @@
+import sqlite3
+from contextlib import closing
+
+database = 'VIP.db'
+
+
+with closing(sqlite3.connect(database)) as connection:
+    with closing(connection.cursor()) as cursor:
+        cursor.execute("CREATE TABLE services (service TEXT, result TEXT, owner TEXT, url TEXT, extra_output TEXT)")
+        rows = cursor.execute("SELECT * FROM services").fetchall()
+        print(rows)
+
+
+#### More useful queries:
+        
+# TRUNCATE TABLE services
+# cursor.execute("ALTER TABLE services ADD date datetime;")
+# cursor.execute("CREATE TABLE services (service TEXT, result TEXT, owner TEXT, url TEXT, extra_output TEXT)")
+# cursor.execute("DELETE FROM services;")
